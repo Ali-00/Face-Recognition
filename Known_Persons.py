e50482c413685d384a64a95fe1ac054b8a0817ea
Ali-00 <73517844+Ali-00@users.noreply.github.com>
2021-06-27 01:18:35 +0500

    Create Known_Persons.py

---
+++ Known_Persons.py
@@ -0,0 +1,23 @@
+import face_recognition
+
+class KnownPersons:
+    def __init__(self):
+        self.known_face_encodings = []
+        self.known_face_names = []
+    def AddEncoding(self,Name):
+        self.image = face_recognition.load_image_file(Name+".jpeg")
+        self.encoding = face_recognition.face_encodings(self.image)[0]
+        self.name = Name
+        self.known_face_encodings.append(self.encoding)
+        self.known_face_names.append(self.name)
+
+KP = KnownPersons()
+
+KP.AddEncoding("Shah Raza")
+KP.AddEncoding("Ali")
+KP.AddEncoding("Hudabia")
+KP.AddEncoding("Irum")
+
+face_locations = []
+face_encodings = []
+face_names = []
